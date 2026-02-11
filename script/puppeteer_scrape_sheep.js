36a3ac3539b4ef3666f6646fe7137d5d5e75a995
mcmanusm <156713818+mcmanusm@users.noreply.github.com>
2026-02-11 16:23:49 +1100

    Update puppeteer_scrape_sheep.js

--- script/puppeteer_scrape_sheep.js
+++ script/puppeteer_scrape_sheep.js
@@ -1,8 +1,3 @@
-- name: Install Chromium
-  run: npx puppeteer browsers install chrome
-
-- name: Run scrape script
-  run: cd script && node puppeteer_scrape_sheep.js
 
 // ============================================================
 // puppeteer_scrape_sheep.js - Sheep Metrics Scraper
